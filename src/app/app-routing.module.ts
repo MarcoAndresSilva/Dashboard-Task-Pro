da70fad2ee69c9cc1a8064abe84febaf97a8b936
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-05 00:33:09 -0400

    feat: crear estructura del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule) }, // Lazy loading se carga en demanda
+  { path: 'stats', loadChildren: () => import('./stats/stats.module').then(m => m.StatsModule) } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
