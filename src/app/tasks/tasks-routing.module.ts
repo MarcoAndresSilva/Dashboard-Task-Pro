323c532a2a01e1ea9b911e811f9b56a1658ccec7
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-06 23:55:11 -0400

    feat: rutas up

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TasksComponent } from './tasks.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 
 const routes: Routes = [
-  { path: '', component: TasksComponent },
   { path: '', component: TaskListComponent },
 ];
 @NgModule({
