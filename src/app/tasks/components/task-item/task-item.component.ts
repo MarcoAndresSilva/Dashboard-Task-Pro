79e09597adc474db75e07dc8254799cdc04c1cc6
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-09 13:53:51 -0400

    feat: agregando ChangueDetectionStrategy.onPush

--- src/app/tasks/components/task-item/task-item.component.ts
+++ src/app/tasks/components/task-item/task-item.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-item',
   standalone: false,
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.scss'
+  styleUrl: './task-item.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export class TaskItemComponent implements OnInit {
 
--- src/app/tasks/components/task-list/task-list.component.ts
+++ src/app/tasks/components/task-list/task-list.component.ts
@@ -1,43 +1,59 @@
-import { Component, OnInit} from '@angular/core';
-import { Task } from '../../models/task.model';
-import { TaskService } from '../../services/task.service';
+  import { Component, OnInit} from '@angular/core';
+  import { Task } from '../../models/task.model';
+  import { TaskService } from '../../services/task.service';
 
-@Component({
-  selector: 'app-task-list',
-  standalone: false,
-  templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
-})
+  @Component({
+    selector: 'app-task-list',
+    standalone: false,
+    templateUrl: './task-list.component.html',
+    styleUrl: './task-list.component.scss'
+  })
 
-export class TaskListComponent implements OnInit {
-  tasks: Task[] = []; // array vacio que se llenara con el servicio
+  export class TaskListComponent implements OnInit {
+    tasks: Task[] = []; // array vacio que se llenara con el servicio
 
-  constructor(private taskService: TaskService) { } // inyectamos el servicio
+    constructor(private taskService: TaskService) { } // inyectamos el servicio
 
-  ngOnInit(): void { 
-    console.log( 'TaskListComponent cargado', this.tasks );
-    this.loadTasks();
-  }
+    ngOnInit(): void { 
+      console.log( 'TaskListComponent cargado', this.tasks );
+      this.loadTasks();
+    }
 
-  loadTasks(): void {
-    this.taskService.getTasks().subscribe(tasksFromService => {
-      console.log('tareas recibidas desde el servicio', tasksFromService);
+    loadTasks(): void {
+      this.taskService.getTasks().subscribe(tasksFromService => {
+        console.log('tareas recibidas desde el servicio', tasksFromService);
+        this.tasks = tasksFromService;
+      });
+    }
+
+    //funcion para cambiar el estado de la tarea
+    handleToggleComplete(taskToUpdate: Task): void {
+        this.tasks = this.tasks.map(task => {
+          if(task.id === taskToUpdate.id){
+          return {...task, completed: !task.completed}; //nuevo oobjeto con la propiedad 'completed' cambiada 
+        }
+        return task // devolvemos el objeto original, la tarea sin cambiar el estado
+      });
+    
+
+      // funcion sin usar onpush
+      // this.taskService.updateTask(taskToUpdate).subscribe(updatedTask =>{
+    //   console.log('tarea actualizada a travez del servicio', updatedTask);
       
-      this.tasks = tasksFromService;
-    });
-  }
-
-  
-  handleToggleComplete(taskToUpdate: Task): void {
-    this.taskService.updateTask(taskToUpdate).subscribe(updatedTask =>{
-      console.log('tarea actualizada a travez del servicio', updatedTask);
-      
-      const index = this.tasks.findIndex(t => t.id === updatedTask.id);
-      if(index !== -1){
-        this.tasks[index] = updatedTask;
-      }
-    });
-
+    //   const index = this.tasks.findIndex(t => t.id === updatedTask.id);
+    //   if(index !== -1){
+    //     this.tasks[index] = updatedTask;
+    //   }
+    // });
   }
   
+
+  trackByTaskId(index: number, task: Task): string | number {
+    return task.id;
+  }
+
+
+
+
+
 }
