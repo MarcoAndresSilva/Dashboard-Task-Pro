8a454fda35ddc73de826e868f060302471a9f2bd
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-09 07:07:49 -0400

    feat: añadiendo nuevo modulo stats

--- src/app/stats/stats-routing.module.ts
+++ src/app/stats/stats-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StatsComponent } from './stats.component';
 
-const routes: Routes = [{ path: '', component: StatsComponent }];
+const routes: Routes = [
+  { path: '', component: StatsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
