d42c28c039184d288b978ff0a172b34b73a47205
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-10 23:14:21 -0400

    feat: añadienod metodos behaviorSubjet ahora empiexa vacio

--- src/app/tasks/services/task.service.ts
+++ src/app/tasks/services/task.service.ts
@@ -16,18 +16,11 @@ interface TodosApiResponse {
 })
 export class TaskService {
 
-  private tasks$ = new BehaviorSubject<Task[]>([ 
-    { id: 1, title: 'Configurar la arquitectura del proyecto', completed: true, description: 'Usar módulos y lazy loading.' },
-    { id: 2, title: 'Crear componente de lista de tareas', completed: true, description: 'Este componente.' },
-    { id: 3, title: 'Crear componente de items de tareas', completed: true, description: 'Otro componente para cada tarea.' },
-    { id: 4, title: 'Añadir comunicación entre componentes', completed: true, description: 'Usar @Input y @Output.' },
-    { id: 5, title: 'Implementar un servicio para las tareas', completed: true, description: 'Mover la lógica a un servicio inyectable.' },
-    { id: 6, title: 'Crear un formulario para agregar tareas', completed: false, description: 'Usar Reactive Forms.' },
-    { id: 7, title: 'Implementar validaciones en el formulario', completed: false, description: 'Usar Validators.' },
-    { id: 8, title: 'Implementar una barra de navegación', completed: false, description: 'Usar Router.' },
-  ]);
+  private apiurl = 'https://dummyjson.com/todos';
 
-constructor() { }
+  private tasks$ = new BehaviorSubject<Task[]>([]); // el behaviorSubject empieza con un array vacio
+
+constructor(private http: HttpClient) { }
 
 // funcion para obtener las tareas, simula una llamada a una API
 getTasks(): Observable<Task[]> {
